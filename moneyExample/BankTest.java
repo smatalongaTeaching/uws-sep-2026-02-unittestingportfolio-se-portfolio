151e03072f3937aa2bca04f35ca4f803821f3d10
Dean Cain <b01787017@studentmail.uws.ac.uk>
2026-02-11 20:26:07 +0000

    Stage 4.2 – added failing test for multiplying sums

--- moneyExample/BankTest.java
+++ moneyExample/BankTest.java
@@ -53,5 +53,30 @@ class BankTest {
         assertEquals(Money.dollar(15), result);
     }
 
+    @Test
+    void testSumOfSumsDifferentCurrencies() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+
+        Expression sum1 = Money.dollar(5).plus(Money.franc(10));  
+        Expression sum2 = Money.dollar(5).plus(Money.franc(10));  
+
+        Expression complex = sum1.plus(sum2); 
+        Money result = bank.convert(complex, "USD");
+
+        assertEquals(Money.dollar(20), result);
+    }
+
+    @Test
+    void testMultiplySumSameCurrency() {
+        Bank bank = new Bank();
+
+        Expression sum = Money.dollar(5).plus(Money.dollar(5)); 
+        Expression product = sum.times(2);
+
+        Money result = bank.convert(product, "USD");
+        assertEquals(Money.dollar(20), result);
+    }
+
 
 }
