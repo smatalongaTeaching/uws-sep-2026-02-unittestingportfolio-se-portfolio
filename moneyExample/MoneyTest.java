ddd21da5e480fdf039afec245bedd318171efb43
Dean Cain <b01787017@studentmail.uws.ac.uk>
2026-02-11 00:26:28 +0000

    Refactor MoneyTest to use assertEquals for assertions and remove unused imports

--- moneyExample/MoneyTest.java
+++ moneyExample/MoneyTest.java
@@ -1,10 +1,8 @@
 
 package moneyExample;
 
-import java.beans.Transient;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class MoneyTest {
@@ -12,18 +10,16 @@ public class MoneyTest {
     @Test
     public void testMoneyCreation() {
         Money money = new Money(100, "USD");
-        assert money.getAmount() == 100;
-        assert money.getCurrency().equals("USD");
+        assertEquals(100, money.getAmount());
+        assertEquals("USD", money.getCurrency());
     }
 
     @Test
     public void testMoneyAdditionofTwoCurencies() {
         Money money1 = new Money(100, "USD");
         Money money2 = new Money(50, "GBP");
-
-        Money result = money1.add(money2);
-        
-        assert result.getAmount() == 175;
-        assert result.getCurrency().equals("USD");
+        Money result = money1.add(money2);        
+        assertEquals(175, result.getAmount());
+        assertEquals("USD", result.getCurrency());
     }
 }
