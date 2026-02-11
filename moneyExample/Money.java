ebfdac45fd2786c9bf2eb8e6227f95ddaed2c0bd
Dean Cain <b01787017@studentmail.uws.ac.uk>
2026-02-11 16:16:32 +0000

    Stage 2.2b – implemented Money factory methods and hierarchy equality (tests passing)

--- moneyExample/Money.java
+++ moneyExample/Money.java
@@ -13,5 +13,22 @@ public abstract class Money {
     }
 
     public abstract String getCurrency();
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Money)) return false;
+        Money other = (Money) obj;
+        return amount == other.amount && getCurrency().equals(other.getCurrency());
+    }
+
 }
 
